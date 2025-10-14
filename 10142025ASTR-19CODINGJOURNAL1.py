410981e598cf9508fa92809b1aec331d8c651ff1
bkolysko <bkolysko@ucsc.edu>
2025-10-14 12:20:33 -0700

    Delete 10142025ASTR-19CODINGJOURNAL1.py

--- 10142025ASTR-19CODINGJOURNAL1.py
+++
@@ -1,6 +0,0 @@
-#this program will print
-#my full name, pronouns, and two sentences about my favorite movie and favorite food
-print("Full Name: Benjamin Kolysko.")
-print("Pronouns:He/Him.")
-print("My favorite movie is any movie that I can watch with my friends that make us laugh."
-      "And my favorite food is any food that tastes good and has good macros.")
