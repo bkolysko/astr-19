97d72583abac36f6da1658207c34b8f4b7f2be73
bkolysko <bkolysko@ucsc.edu>
2025-10-14 14:07:00 -0700

    Coding Journal 1 Prompt 2

---
+++ 10142025ASTR-19CODINGJOURNAL2.py
@@ -0,0 +1,21 @@
+#First, I will assign variables to the floating numbers and integers
+x = 3.77
+y = 5.0
+i1 = 2
+i2 = 8
+
+#Second, I will print the sum of two floating point numbers and print the data type of the answer
+print(x+y)
+print(type(x+y))
+
+#Third, I will print the difference between two integers and print the data type of the answer
+print(i1-i2)
+print(type(i1-i2))
+
+#Finally, I will print the product of a floating point number and an integer
+#and I will print the data type of the answer
+print(x*i2)
+print(type(x*i2))
+
+
+
