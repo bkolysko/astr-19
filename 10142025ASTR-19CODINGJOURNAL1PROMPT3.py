74253a1f7f84d294698f002d7a053b36189eecfb
bkolysko <bkolysko@ucsc.edu>
2025-10-14 14:31:28 -0700

    Coding Journal 1 Prompt 3

---
+++ 10142025ASTR-19CODINGJOURNAL1PROMPT3.py
@@ -0,0 +1,11 @@
+#First I will define f(x)
+def f(x):
+    return x**3+8
+#Then I will define the main function and print the result
+def main():
+    result = f(9)
+    print(result)
+    if result > 27:
+        print("YAY!")
+main()
+
